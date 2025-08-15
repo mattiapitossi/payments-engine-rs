030e57d612ebdcf9a592fbd873d9ef16e8b0f644
Mattia <mattiapitossi@icloud.com>
2025-08-15 11:39:21 +0200

    feat: add file input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+
+use csv::{ReaderBuilder, Trim::All};
+
+use crate::domain::Transaction;
+
+mod domain;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut rdr = ReaderBuilder::new()
+        .trim(All) // as we want to accept csv with with whitespaces
+        .from_reader(io::stdin());
+
+    for result in rdr.deserialize() {
+        let record: Transaction = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
